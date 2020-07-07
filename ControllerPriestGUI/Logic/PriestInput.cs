e9c37c951cb19cdf5ae9ca73cb8b20bd954372ca
Terminality <me@terminality.com>
2020-07-07 20:44:52 +0100

    Add project files.

---
+++ ControllerPriestGUI/Logic/PriestInput.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using JetBrains.Annotations;
+using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+using SharpDX.XInput;
+
+namespace ControllerPriestGUI.Logic
+{
+    class PriestInput
+    {
+        private Controller[] controllers;
+        private int master = -1;
+        private int output = -1;
+        private ViGEmClient client;
+        private IXbox360Controller xController;
+        private Gamepad masterSwitchState;
+        private int lastPacketNum = -1;
+
+        public int Output { get => output; set => output = value; }
+        public int Master { get => master; set => master = value; }
+
+        /// <summary>
+        /// Startup PriestInput class.
+        /// </summary>
+        public void Start()
+        {
+            client = new ViGEmClient();
+            xController = client.CreateXbox360Controller();
+            controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
+
+            masterSwitchState.Buttons = (GamepadButtonFlags.LeftShoulder | GamepadButtonFlags.RightShoulder | GamepadButtonFlags.RightThumb | GamepadButtonFlags.LeftThumb);
+        }
+
+        /// <summary>
+        /// Run update loop to pass controller state to the output controller. As well as check if new master command has been triggered.
+        /// </summary>
+        public void Update()
+        {
+            if (output != -1 && master != -1)
+            {
+                if (master != -1 && controllers[master].IsConnected && controllers[master].GetState().PacketNumber != lastPacketNum)
+                {
+                    State currState = controllers[master].GetState();
+                    lastPacketNum = currState.PacketNumber;
+
+                    if (ChangeMasterTriggered(currState))
+                    {
+                        ChangeMaster();
+                    }
+                    else
+                    {
+
+                        xController.SetButtonState(Xbox360Button.A, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A));
+                        xController.SetButtonState(Xbox360Button.B, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B));
+                        xController.SetButtonState(Xbox360Button.X, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.X));
+                        xController.SetButtonState(Xbox360Button.Y, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y));
+
+                        xController.SetButtonState(Xbox360Button.Up, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp));
+                        xController.SetButtonState(Xbox360Button.Down, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown));
+                        xController.SetButtonState(Xbox360Button.Left, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft));
+                        xController.SetButtonState(Xbox360Button.Right, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight));
+
+                        xController.SetButtonState(Xbox360Button.Start, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Start));
+                        xController.SetButtonState(Xbox360Button.Back, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Back));
+
+                        xController.SetButtonState(Xbox360Button.RightThumb, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb));
+                        xController.SetButtonState(Xbox360Button.LeftThumb, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb));
+
+                        xController.SetButtonState(Xbox360Button.LeftShoulder, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftShoulder));
+                        xController.SetButtonState(Xbox360Button.RightShoulder, currState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightShoulder));
+
+                        xController.SetAxisValue(Xbox360Axis.LeftThumbX, currState.Gamepad.LeftThumbX);
+                        xController.SetAxisValue(Xbox360Axis.LeftThumbY, currState.Gamepad.LeftThumbY);
+                        xController.SetAxisValue(Xbox360Axis.RightThumbX, currState.Gamepad.RightThumbX);
+                        xController.SetAxisValue(Xbox360Axis.RightThumbY, currState.Gamepad.RightThumbY);
+                        xController.SetSliderValue(Xbox360Slider.LeftTrigger, currState.Gamepad.LeftTrigger);
+                        xController.SetSliderValue(Xbox360Slider.RightTrigger, currState.Gamepad.RightTrigger);
+
+                        xController.SubmitReport();
+                    }
+
+                }
+                else if (master != -1 && !controllers[master].IsConnected)
+                {
+                    ChangeMaster();
+
+                    if (master != -1 && !controllers[master].IsConnected)
+                    {
+                        master = -1;
+                    }
+                }
+            }
+            else if (output != -1 && master == -1)
+            {
+                //If no master has been set, but output is set we should see if any controller wants to manually grab control.
+                for (int i = 0; i < controllers.Length; i++)
+                {
+                    if (i != output)
+                    {
+                        if (controllers[i].IsConnected)
+                        {
+                            if (ChangeMasterTriggered(controllers[i].GetState()))
+                            {
+                                master = i;
+                            }
+                        }    
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Try to find a new master controller that isn't the output controller. If no other potential master is found,
+        /// then 
+        /// </summary>
+        public void ChangeMaster()
+        {
+            bool newControlFound = false;
+
+            for (int i = master + 1; i != master && !newControlFound; i++)
+            {
+                if (i >= 4)
+                    i = 0;
+
+                if (controllers[i].IsConnected && i != output)
+                {
+                    master = i;
+                    newControlFound = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check what ports have controllers connected to it.
+        /// </summary>
+        /// <returns>bool array showing the status of the controller ports. True = Connected | False = Disconnected</returns>
+        public bool[] CheckConnections()
+        {
+            var results = new[] { false, false, false, false };
+
+            for (int i = 0; i < controllers.Length; i++)
+            {
+                if (controllers != null)
+                {
+                    results[i] = controllers[i].IsConnected;
+                }
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Allow an external class to start the emulated controller.
+        /// </summary>
+        public void StartOutputController()
+        {
+            xController.Connect();
+        }
+
+        /// <summary>
+        /// Allow external class to stop the emulated controller.
+        /// </summary>
+        public void StopOutputController()
+        {
+            xController.Disconnect();
+        }
+
+        /// <summary>
+        /// Allow external class to check if a controller is connected.
+        /// </summary>
+        /// <param name="index">The controller port number (ports start at 0)</param>
+        /// <returns></returns>
+        public bool IsConnected(int index)
+        {
+            return controllers[index].IsConnected;
+        }
+
+        /// <summary>
+        /// Check to see if the state of a controller has requested a change in master.
+        /// </summary>
+        /// <param name="state">The current state of a controller</param>
+        /// <returns></returns>
+        public bool ChangeMasterTriggered(State state)
+        {
+
+            if (state.Gamepad.Buttons == masterSwitchState.Buttons)
+                return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ ControllerPriestGUI/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using ControllerPriestGUI.Logic;
+
+namespace ControllerPriestGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PriestInput input = new PriestInput();
+        SolidColorBrush disconnectedColour = new SolidColorBrush(Color.FromRgb(230, 0, 0));
+        SolidColorBrush connectedColour = new SolidColorBrush(Color.FromRgb(0, 230, 0));
+        Label[] con_status = new Label[4];
+        String strConnected = "CONNECTED";
+        String strDisconnected = "DISCONNECTED";
+        String strStart = "START";
+        String strStop = "STOP";
+        String strOutput = "OUTPUT";
+        String strMaster = "MASTER";
+        String WarnOutputMaster = "You can't set the master controller to be the same as the output controller! Please choose another controller slot!";
+        String WarnMasterDisconnect = "No controller is connected on this port! Please choose another controller port to be master.";
+        String WarnErrorType = "Controller Error";
+        bool emulatedStart = false;
+        
+        /// <summary>
+        /// Initialise the MainWaindow. Setup combobox and setup arrays for referencing later.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Start();
+            input.Update();
+            PriestInputUpdate();
+
+            con_status[0] = lbl_con_1_status;
+            con_status[1] = lbl_con_2_status;
+            con_status[2] = lbl_con_3_status;
+            con_status[3] = lbl_con_4_status;
+
+            con_master_combo.Items.Add("1");
+            con_master_combo.Items.Add("2");
+            con_master_combo.Items.Add("3");
+            con_master_combo.Items.Add("4");
+            con_master_combo.Items.Add("NONE");
+            con_master_combo.SelectedIndex = 4;
+            lbl_status.Content = "!!!REMEMBER!!! Giving control has changed to R1 + L1 + L3 + R3 !!!REMEMBER!!!";
+        }
+
+        /// <summary>
+        /// Used when starting or stopping the controller to connect or disconnect the controller that will be used as output.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ActivateEmulatedController(object sender, RoutedEventArgs e)
+        {
+            if (!emulatedStart)
+            {
+                var initialCheck = input.CheckConnections();
+                input.StartOutputController();
+                var secondaryCheck = input.CheckConnections();
+
+                for (int i = 0; i < initialCheck.Length; i++)
+                {
+                    if (!initialCheck[i] && secondaryCheck[i])
+                    {
+                        input.Output = i;
+                    }
+                }
+
+                emulatedStart = true;
+
+                btn_emu_con.Content = strStop;
+            }
+            else
+            {
+                emulatedStart = false;
+                btn_emu_con.Content = strStart;
+                input.StopOutputController();
+                input.Output = -1;
+            }
+        }
+
+        /// <summary>
+        /// Update the labels on the window involved with controller connections. Set text and colour.
+        /// </summary>
+        /// <param name="connections"></param>
+        public void UpdateConnectionLabels(bool[] connections)
+        {
+
+            for (int i = 0; i < con_status.Length; i++)
+            {
+                con_status[i].Content = (connections[i] ? strConnected : strDisconnected);
+                con_status[i].Foreground = (connections[i] ? connectedColour : disconnectedColour);
+            }
+
+            if (input.Output != -1 && connections[input.Output])
+                con_status[input.Output].Content = strOutput;
+            if (input.Master != -1 && connections[input.Master])
+                con_status[input.Master].Content = strMaster;
+        }
+
+        /// <summary>
+        /// Our background task that helps process the input of controllers. We recheck connection to controllers and also check to see if the
+        /// master controller has been changed by PriestInput class. If so we update the ComboBox to reflect the new master controller port.
+        /// </summary>
+        async void PriestInputUpdate()
+        {
+            while (true)
+            {
+                await Dispatcher.Yield(DispatcherPriority.ApplicationIdle);
+                UpdateConnectionLabels(input.CheckConnections());
+                input.Update();
+
+                if (input.Master != con_master_combo.SelectedIndex)
+                    if (input.Master == -1 && con_master_combo.SelectedIndex != 4)
+                        con_master_combo.SelectedIndex = 4;
+                    else if (input.Master != -1)
+                        con_master_combo.SelectedIndex = input.Master;
+
+            }
+        }
+
+        /// <summary>
+        /// When user selects a new option in the combobox, this code will run checks on the selected option to make sure it's not invalid. 
+        /// An invalid state is one where the user attempts to select as master the emulated controller or a port that has no controller selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MasterControllerSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (input.Output != -1 && con_master_combo.SelectedIndex == input.Output)
+            {
+                MessageBox.Show(WarnOutputMaster, WarnErrorType, MessageBoxButton.OK, MessageBoxImage.Error);
+                con_master_combo.SelectedIndex = 4;
+            }
+            else if (con_master_combo.SelectedIndex < 4 && !input.IsConnected(con_master_combo.SelectedIndex))
+            {
+                MessageBox.Show(WarnMasterDisconnect, WarnErrorType, MessageBoxButton.OK, MessageBoxImage.Error);
+                con_master_combo.SelectedIndex = 4;
+            }
+            else
+            {
+                if (con_master_combo.SelectedIndex == 4)
+                    input.Master = -1;
+                else
+                    input.Master = con_master_combo.SelectedIndex;
+            }
+        }
+    }
+}
